7a3abc885500b1f1dd5e730b1ef4509057da4569
Ramakrishna Koneru <Ramakrishna.Koneru@gmail.com>
2020-06-08 21:53:06 -0500

    Rama - Initial commit.

---
+++ src/main/java/com/rkoneru/githubeventsservice/GithubEventsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkoneru.githubeventsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubEventsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubEventsServiceApplication.class, args);
+	}
+
+}
